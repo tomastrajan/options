9fe86047590d325878f535cef0bf736da0b9fc24
tomastrajan <tomas.trajan@gmail.com>
2017-07-05 15:19:58 +0200

    fix: about tests

--- src/app/static/about/about.component.spec.ts
+++ src/app/static/about/about.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedModule } from '@app/shared';
+import { CoreModule } from '@app/core';
+
 import { AboutComponent } from './about.component';
 
 describe('AboutComponent', () => {
@@ -9,7 +12,7 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [NoopAnimationsModule],
+      imports: [SharedModule, CoreModule, NoopAnimationsModule],
       declarations: [AboutComponent]
     })
       .compileComponents();
