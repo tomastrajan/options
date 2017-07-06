10e46dd5a4f01eb2215c9341dd579b1479848a96
tomastrajan <tomas.trajan@gmail.com>
2017-07-06 12:57:03 +0200

    feat: randomize data to prevent breaching of TOS

--- src/app/core/resource/yahoo.service.ts
+++ src/app/core/resource/yahoo.service.ts
@@ -21,6 +21,7 @@ export class YahooService {
     const url = `${API_OPTIONS}${symbol}${date ? '?date=' + date : ''}`;
     return this.http.get(url)
       .map(res => res.json().optionChain.result[0])
+      .map(data => this.dataRandomizer(data))
       .catch(err => {
         console.log(err);
         return Observable.of([])
@@ -34,6 +35,54 @@ export class YahooService {
         console.log(err);
         return Observable.of([])
       });
+  }
+
+  dataRandomizer(data) {
+    // to comply with yahoo TOS,
+    // app serves as a technological prototype not a real stock application
+    // only mock randomized data is displayed
+    [
+      'ask',
+      'regularMarketChange',
+      'regularMarketChangePercent',
+      'postMarketPrice'
+    ].forEach(key =>
+      data.quote[key] = YahooService.numberRandomizer(data.quote[key]));
+
+    if (data.options && data.options[0]) {
+      const KEYS = [
+        'bid',
+        'ask',
+        'impliedVolatility',
+        'lastPrice',
+        'volume',
+        'change',
+        'percentChange'
+      ];
+      const { calls, puts } = data.options[0];
+      calls.forEach(call => {
+        KEYS.forEach(key => {
+          const val = call[key];
+          call[key] = YahooService.numberRandomizer(call[key]);
+          console.log(call.contractSymbol, key, val, call[key]);
+        })
+      });
+      puts.forEach(put => KEYS
+        .forEach(key => put[key] = YahooService.numberRandomizer(put[key])));
+    }
+
+    return data;
+  }
+
+  static numberRandomizer(value: any) {
+    value = parseFloat(value);
+    // randomize value by 5 to 15%;
+    value = value * (1 + YahooService.getRandomRange(5, 15) / 100);
+    return parseFloat(value.toFixed(2));
+  }
+
+  static getRandomRange(min, max) {
+    return Math.random() * (max - min) + min;
   }
 
 }
